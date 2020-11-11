98a7580ba6a350f45fa241a721cbef370d380b01
Aleks <shenshin@me.com>
2020-11-11 09:39:21 +0100

    uninstall prettier

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import express from "express";
-// rest of the code remains same
-const app = express();
-const PORT = 8000;
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-});
+console.log('hello world');
